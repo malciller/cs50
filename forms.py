2315df2b05166bdd6cdb085279ebad815cfdd231
ANSWERNETNETWOR/cmiller <malciller@gmail.com>
2023-11-28 23:37:34 -0500

    some design implemented, completely broke chat display

--- forms.py
+++ forms.py
@@ -3,11 +3,16 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)],
+                           render_kw={"placeholder": "Username"})
+    email = StringField('Email', validators=[DataRequired(), Email()],
+                        render_kw={"placeholder": "Email"})
+    password = PasswordField('Password', validators=[DataRequired()],
+                             render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')],
+                                     render_kw={"placeholder": "Confirm Password"})
     submit = SubmitField('Sign Up')
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)], 
